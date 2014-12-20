c3da96a17e2b85f26486e842660fa3744cac177c
Bryan L. Venable <bryanlvenable@gmail.com>
2014-12-20 10:00:52 -0800

    Progress on watchout

--- watchout.js
+++ watchout.js
@@ -1,6 +1,7 @@
 // start slingin' some d3 here.
 (function(){
-  var gameOptions, gameStats, axes, gameBoard, updateScore, updateHighScore, Player;
+  var gameOptions, gameStats, axes, gameBoard, updateScore, updateHighScore,
+    Player, createEnemies, render, enemy, startPosition, endPosition;
 
   gameOptions = {
     height: 450,
@@ -11,7 +12,8 @@
 
   gameStats = {
     score: 0,
-    highScore: 0
+    highScore: 0,
+    collisions: 0
   };
 
   axes = {
@@ -19,39 +21,59 @@
     y: d3.scale.linear().domain([0,100]).range([0,gameOptions.height])
   };
 
-  gameBoard = d3.select('.field').append('svg:svg').attr('width', gameOptions.width).attr('height', gameOptions.height);
+  gameBoard = d3.select('.field').append('svg:image')
+    .attr('xlink:href','http://www.publicdomainpictures.net/pictures/10000/nahled/starry-night-871287410454UmtH.jpg')
+    .attr('width', gameOptions.width)
+    .attr('height', gameOptions.height);
 
   updateScore = d3.select('#current-score').text(gameStats.score.toString());
 
-  updateHighScore = function(){
-    gameStats.highScore = _.max(gameStats.highScore, gameStats.score);
-    return d3.select('high-score').text(gameStats.highScore.toString());
-  };
+  updateHighScore = d3.select('#high-score').text(gameStats.highScore.toString());
 
-  createEnemies = function() {
-    return_.range(0, gameOptions.numberEnemies).map(function(i) {
-      return {
-        id: i,
-        x: Math.random() * 100,
-        y: Math.random() * 100
-      };
-    });
-  };
+  updateCollision = d3.select('#collisions').text(gameStats.collisions.toString());
 
-  render = function(enemyData) {
-    var enemies;
+//   updateHighScore = function(){
+//     gameStats.highScore = _.max(gameStats.highScore, gameStats.score);
+//     return d3.select('high-score').text(gameStats.highScore.toString());
+//   };
 
-    enemies = gameBoard.selectAll('circle.enemy').data(enemyData, function(d) {
-      return d.id;
-    });
+//   createEnemies = function() {
+//     return_.range(0, gameOptions.numberEnemies).map(function(i) {
+//       return {
+//         id: i,
+//         x: Math.random() * 100,
+//         y: Math.random() * 100
+//       };
+//     });
+//   };
 
-    enemies.enter().append('svg:circle').attr('class', 'enemy').attr('cx', function(enemy) {
-      return axes.x(enemy.x);
-    }).attr('cy', function(enemy) {
-      return axes.y(enemy.y);
-    }).attr('r', 0);
-    enemies.exit().remove();
-  };
+//   render = function(enemyData) {
+//     var enemies;
+
+//     enemies = gameBoard.selectAll('circle.enemy').data(enemyData, function(d) {
+//       return d.id;
+//     });
+
+//     enemies.enter().append('svg:circle').attr('class', 'enemy')
+//     .attr('cx', function(enemy) {
+//       return axes.x(enemy.x);
+//     }).attr('cy', function(enemy) {
+//       return axes.y(enemy.y);
+//     }).attr('r', 0);
+//     enemies.exit().remove();
+//   };
+
+
+
+//   play = function() {
+//     var gameTurn, increaseScore;
+//     gameTurn = function() {
+//       var newEnemyPositions;
+//       newEnemyPositions = createEnemies();
+//       return render(newEnemyPositions);
+//     };
+//   };
+//   play();
 }).call(this);
 
 
