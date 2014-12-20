ac60caf019b647ba981b21b337c26a0e02641331
Bryan L. Venable <bryanlvenable@gmail.com>
2014-12-19 19:18:30 -0800

    Setup the environment

--- watchout.js
+++ watchout.js
@@ -1,2 +1,19 @@
 // start slingin' some d3 here.
+(function(){
+  var gameOptions, gameStats;
 
+  gameOptions = {
+    height: 450,
+    width: 700,
+    numberEnemies: 30,
+    padding: 20
+  }
+
+  gameStats = {
+    score: 0,
+    highScore
+  }
+
+
+
+}).call(this);
