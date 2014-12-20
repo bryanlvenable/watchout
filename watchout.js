2d8a495231c837678034b8da61c2c68dfade0760
Bryan L. Venable <bryanlvenable@gmail.com>
2014-12-19 19:35:05 -0800

    Set up game board

--- watchout.js
+++ watchout.js
@@ -7,13 +7,24 @@
     width: 700,
     numberEnemies: 30,
     padding: 20
-  }
+  };
 
   gameStats = {
     score: 0,
-    highScore
-  }
+    highScore: 0
+  };
 
+  axes = {
+    x: d3.scale.linear().domain([0,100]).range([0,gameOptions.width]),
+    y: d3.scale.linear().domain([0,100]).range([0,gameOptions.height])
+  };
 
+  gameBoard = d3.select('.field').append('svg:svg').attr('width', gameOptions.width).attr('height', gameOptions.height);
 
+  updateScore = d3.select('#current-score').text(gameStats.score.toString());
+
+  updateHighScore = function(){
+    gameStats.highScore = _.max(gameStats.highScore, gameStats.score);
+    return d3.select('high-score').text(gameStats.highScore.toString());
+  };
 }).call(this);
